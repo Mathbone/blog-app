1241b4eb9462a463b79f284ba0f93622f386803f
Brandon Wilson <pandamathium@gmail.com>
2019-02-17 15:18:48 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php
+echo "Hello World!";
+?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Wilson Blog App</title>
+  </head>
+  <body>
+    <h1>Blog App</h1>
+  </body>
+</html>
