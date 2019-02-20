1aa0a5b214150a53cc2ceadf9c0b05284edfe2bb
Brandon Wilson <pandamathium@gmail.com>
2019-02-19 19:11:00 -0700

    login/logout working

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -25,15 +29,23 @@
           <li><a href="#">Contact</a></li>
         </ul>
         <span class="regbar">
-          <form class="login" action="includes/login.inc.php" method="post">
-            <input type="text" name="login-username" placeholder="Username">
-            <input type="text" name="login-pwd" placeholder="Password">
-            <button type="submit" name="login-submit">Login</button>
-          </form>
-          <a href="register.php">Register</a>
-          <form class="logout" action="includes/logout.inc.php" method="post">
-            <button type="submit" name="logout-submit">Logout</button>
-          </form>
+          <?php
+          if(isset($_SESSION['userId'])) {
+            echo '<form class="logout" action="includes/logout.inc.php" method="post">
+              <button type="submit" name="logout-submit">Logout</button>
+            </form>';
+          }
+          else{
+            echo '<form class="login" action="includes/login.inc.php" method="post">
+              <input type="text" name="login-username" placeholder="Username">
+              <input type="password" name="login-pwd" placeholder="Password">
+              <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="register.php">Register</a>';
+          }
+          ?>
+
+
         </span>
       </nav>
       <div id="side-menu" class="side-nav">
--- includes/register.inc.php
+++ includes/register.inc.php
@@ -31,8 +31,38 @@ if (isset($_POST['register-submit'])){
       header("Location: ../register.php?error=sqlerror&uid=".$username."&email=".$email);
       exit();
     }
+    else{
+      mysqli_stmt_bind_param($stmt, "s", $username);
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
+      $resultCheck = mysqli_stmt_num_rows($stmt);
+      if($resultCheck > 0){
+        header("Location: ../register.php?error=usertaken&uid=".$username."&email=".$email);
+        exit();
+      }
+      else{
+        $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+          header("Location: ../register.php?error=sqlerror&uid=".$username."&email=".$email);
+          exit();
+        }
+        else{
+          $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+          mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+          mysqli_stmt_execute($stmt);
+          header("Location: ../register.php?signup=success");
+          exit();
+        }
+      }
+    }
   }
-
-
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
+}
+else{
+  header("Location: ../register.php");
+  exit();
 }
 ?>
