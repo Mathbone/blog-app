3348c0061d4a48e1ced724018c6e696918cf83c4
Brandon Wilson <pandamathium@gmail.com>
2019-02-21 18:31:07 -0700

    displaying posts

---
+++ error.php
@@ -0,0 +1,12 @@
+<?php
+  require "header.php";
+?>
+ <main id="main">
+   <h1>There is an SQL error preventing you from using this page.</h1>
+
+ </main>
+
+
+<?php
+  require 'footer.php';
+?>
--- index.php
+++ index.php
@@ -4,10 +4,24 @@
  <main id="main">
     <?php
     if (isset($_SESSION['userId'])){
+      require 'includes/dbh.inc.php';
 
-      for ($i=0; $i <10 ; $i++) {
-        echo "Blog post: ".($i+1)."<br>";
+      $sql = "SELECT authoridPosts, textPosts, timestampPosts FROM posts ORDER BY timestampPosts DESC LIMIT 10";
+      $stmt = mysqli_stmt_init($conn);
+      if(!mysqli_stmt_prepare($stmt,$sql)){
+        header("Location: ./error.php?error=prepare");
+        exit();
       }
+      else{
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while($row = mysqli_fetch_assoc($result)){
+            echo '<div class="blog-post">'.$row['textPosts'].'<div class="author">'.$row['authoridPosts'].'</div><div class="timestamp">'.$row['timestampPosts'].'</div></div>';
+        }
+
+      }
+
+
     }
     else{
       echo '<h1 class="register-message">Please register or login to view blog posts.</h1>';
